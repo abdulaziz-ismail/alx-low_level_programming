5d225da9e6ad9401614adbd9184c18e5f19efeac
abdulaziz-ismail <abdulismail126@gmail.com>
2023-02-17 20:02:55 -0800

    alphanumber

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-
 /**
- * main - print lower and upper alpha
- * Return: Always 0
+ * main - prints the alphabet in lowercase, and then in uppercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -11,11 +11,8 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
-
-	for (ch = 'A'; ch <= 'Z', ch++)
+	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar(ch);
-
 	putchar('\n');
-
 	return (0);
 }
