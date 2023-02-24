4b75f86ae1d10d7c7ed612b8cd8d121e14d1194f
abdulaziz-ismail <abdulismail126@gmail.com>
2023-02-24 04:15:49 -0800

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * 9_times_table - print the 9 times table
+ */
+void 9_times_tablee(void)
+{
+	int num, mult, prod;
+
+	for (bum = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_put((prod / 10) * '0');
+
+			_putchar((prod % 10) * '0');
+		}
+		_putchar('\n');
+	}
+}
